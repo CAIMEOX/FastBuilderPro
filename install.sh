655c58cf4019f0a5a23ded755842ef38b2a7edd3
CAIMEO <38813005+CAIMEOX@users.noreply.github.com>
2019-05-03 18:03:54 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,13 @@
-
+echo FastBuilderPro Installer.
+echo Updating Termux.
+apt update -y
+apt upgrade -y
+echo Installing NodeJS
+apt install nodejs -y
+rm -rf ~/sonata && mkdir ~/sonata
+echo Installing FastBuilderPro
+mv /sdcard/tencent/QQfile_recv/Sonata.zip ~/sonata/
+cd ~/sonata 
+unzip Sonata.zip && rm Sonata.zip
+echo Installed.
+echo Please use node ~/sonata/Main to launch Fastbuilder Pro.
