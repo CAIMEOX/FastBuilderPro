d675e6b476ed70fc56ca9aec99ea5dde2227c72b
CAIMEO <38813005+CAIMEOX@users.noreply.github.com>
2019-06-30 16:49:31 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,9 +9,11 @@ apt install nodejs -y
 rm -rf ~/sonata && mkdir ~/thor
 cd thor
 echo 正在下载脚本
-wget https://coding.net/u/CAIMEO/p/FastBuilder/git/raw/master/Thor.js
-wget https://coding.net/u/CAIMEO/p/FastBuilder/git/raw/master/package.json
+wget coding.net/u/CAIMEO/p/FastBuilder/git/raw/master/Thor.js
+wget coding.net/u/CAIMEO/p/FastBuilder/git/raw/master/package.json
 echo 正在添加依赖项
 npm i
+echo 正在更新依赖项
+npm update
 echo 安装成功
 echo -e "请使用 \033[32m \"cd thor && node Main\" \033[0m 来启动　FastBuilderPro"
